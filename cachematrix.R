cb943755e3c06f84eeb1ccba4bf48b96bce46964
Florian Pereme (home) <florian.pereme@gmail.com>
2014-09-20 17:44:36 +0200

    Commit my own cachematrix function on github for the coursera R programming assignemnt 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,48 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+##                                 This is the cache and solved R function
+## how to use:
+## set a new variable "here is the a varible" with the make Cache function, with :
 
-makeCacheMatrix <- function(x = matrix()) {
+##        a<-makeCacheMatrix()
+
+## set the matrix content with:
+
+##        a$set(matrix(1:4,2,2)) 
+
+## you can also use coerce to use non sequential numeric sequence for the matrix e.g "a$set(matrix(c(1,2,0,4),2,2))"
+## Use the cache solve function to show the solved cache matrix with:
+
+## cacheSolve(a)
+
+## it returns the cache solved matrix like
+##[,1] [,2]
+##[1,]  1.0 0.00
+##[2,] -0.5 0.25
+
+makeCacheMatrix <- function(x = matrix())                        ## define the cache function 
+{
+     m <- NULL                                                   ## create an undefined variable
+     set <- function(y)                                          ## take a matrix "y" and set it as global
+     {
+          x <<- y
+          m <<- NULL
+     }
+     get<-function() x                                           ## get the "set" matrix
+     setmat<-function(solve) m<<- solve                          ## set the m variable with the solved matrix
+     getmat<-function() m                                        ## get the "cache inverse
+     list(set=set, get=get,setmat=setmat, getmat=getmat)         ## return the attribute list used
 
 }
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x=matrix(), ...)                          ## Solving the cached matrix function
+{
+     m<-x$getmat()                                               ##store the solved matrix in a "m" variable
+     if(!is.null(m))                                             ## check if the "m"matrix isn't empty
+     {
+          message("getting cached data")                         ## display message if the solved matrix is found
+          return(m)                                              ## takes the cached matrix
+     }
+     matrix <- x$get()                                           ## if no matrix cached, do it with the attribute"get" from the list                                                    
+     m<-solve(matrix, ...)                                       ## solve the matrix and store it in a variable
+     x$setmat(m)                                                 ## set the cached variable with the new solved matrix
+     m                                                           ## display the solved matrix
 }
